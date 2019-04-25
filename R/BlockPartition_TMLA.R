2dd6f984cfe1301fb963b65b198588ce92df1c19
Unknown <andrefaandrade@gmail.com>
2019-04-25 16:17:53 -0300

    Fix Geo_Buf bugging at Geographical Partitions

--- R/BlockPartition_TMLA.R
+++ R/BlockPartition_TMLA.R
@@ -8,7 +8,8 @@ BlockPartition_TMLA <- function(evnVariables = NA,
            DirSave = DirB,
            DirM = DirM,
            MRst = sp_accessible_area,
-           type = TipoMoran) {
+           type = TipoMoran,
+           Geo_Buf = Geo_Buf) {
     
   # RecordsData: matrix or data frame with presences records 
   # N: 2 (dafault). interger  Number of group for data  paritioning 
@@ -86,7 +87,8 @@ BlockPartition_TMLA <- function(evnVariables = NA,
   registerDoParallel(cl)
   
   # LOOP----
-  results <- foreach(s = 1:length(RecordsData), .packages = c("raster","modEvA","ape","dismo")) %dopar% {
+  results <- foreach(s = 1:length(RecordsData), .packages = c("raster","modEvA","ape","dismo"),
+                     .export=c("Geo_Buf")) %dopar% {
   # for(s in 1:length(RecordsData)){
   print(paste(s, SpNames[s]))
   # Extract coordinates----
