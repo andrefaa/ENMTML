a251393c31640901759c043bd526ffda57f3ac78
Unknown <andrefaandrade@gmail.com>
2019-04-26 09:52:18 -0300

    Fix to FitENM bugging

--- R/FitENM_Parallel_TMLA.R
+++ R/FitENM_Parallel_TMLA.R
@@ -257,7 +257,7 @@ FitENM_TMLA_Parallel <- function(RecordsData,
   # Construction of models LOOP-----
   results <- foreach(s = 1:length(spN), .packages = c("raster","dismo","kernlab","randomForest",
                                                       "maxnet","maxlike","GRaF","ecospat","plyr","gam","RStoolbox",
-                                                      "adehabitatHS","caret","visreg"),
+                                                      "adehabitatHS","caret","visreg","glmnet"),
                      .export=c("Validation2_0","STANDAR","maxnet2","predict.graf.raster","PCA_ENS_TMLA","predict.maxnet",
                                "Eval_Jac_Sor_TMLA","Validation_Table_TMLA","Thresholds_TMLA","VarImp_RspCurv","hingeval")) %dopar% {
 
