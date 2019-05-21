2f7e7bebba09b92188440a95622ffe051d55ad4a
Unknown <andrefaandrade@gmail.com>
2019-05-21 15:06:44 -0300

    Fix GEO_ENV_KM bugging

--- R/ENM_TheMetaLandALFA.R
+++ R/ENM_TheMetaLandALFA.R
@@ -1205,22 +1205,22 @@ ENMs_TheMetaLand <- function(pred_dir,
               }
               
               absencesTR.0 <- KM(rasterToPoints(SpMask)[,-3],
-                 mask(envTT, SpMask),
+                 mask(envT, SpMask),
                  ((1 / pres_abs_ratio)*nrow(occTR[[i]])))
               absencesTS.0 <- KM(rasterToPoints(SpMaskP)[,-3],
-                 mask(envTT, SpMaskP),
+                 mask(envT, SpMaskP),
                  ((1 / pres_abs_ratio)*nrow(occTS[[i]])))
             }else{
               absencesTR.0 <- KM(cell_coord = rasterToPoints(pseudo.mask)[,-3],
-                                 variable = mask(envTT, pseudo.mask),
+                                 variable = mask(envT, pseudo.mask),
                                  NumAbsence = (1 / pres_abs_ratio)*nrow(occTR[[i]]))
               if(eval_occ=="Y"){
                 absencesTS.0 <- KM(rasterToPoints(pseudo.maskP)[,-3],
-                                   mask(envTT, pseudo.mask),
+                                   mask(envT, pseudo.mask),
                                    (1 / pres_abs_ratio)*nrow(occTS[[i]]))
               }else{
                 absencesTS.0 <- KM(rasterToPoints(pseudo.maskP)[,-3],
-                                   mask(envTT, pseudo.mask),
+                                   mask(envT, pseudo.mask),
                                    (1 / pres_abs_ratio)*nrow(occTS[[i]]))
               }
             }
