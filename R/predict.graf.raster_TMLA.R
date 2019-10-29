7fb0c2ebeda2fd6a023e23ed70b54eca5a964ba6
sjevelazco <sjevelazco@gmail.com>
2019-10-29 09:22:25 -0300

    change space in predict.graf.raster.R

--- R/predict.graf.raster_TMLA.R
+++ R/predict.graf.raster_TMLA.R
@@ -1,4 +1,4 @@
-#written by Santiago Velazco
+#source https://github.com/goldingn/GRaF/blob/master/R/predict.graf.raster.R
 
 # Predict Raster wiht GRaF package GAUSS algorithm
 predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
@@ -19,12 +19,12 @@ predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
     }
   }
   ncols <- ncol(out)
-  
+
   firstrow <- 1
   firstcol <- 1
-  
+
   if (!canProcessInMemory(out, 3)) {
-    
+
     filename <- rasterTmpFile()
     out <- writeStart(out, filename = filename, ...)
     inMemory <- FALSE
@@ -32,20 +32,20 @@ predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
     v <- array(NA, dim = c(nrow(out), ncol(out), nlayers(out)))
     inMemory <- TRUE
   }
-  
+
   tr <- blockSize(out, n = nlayers(x) + 2)
   pb <- pbCreate(tr$n)
-  
+
   for (i in 1:tr$n) {
     rr <- firstrow + tr$row[i] - 1
-    
+
     rowvals <- getValuesBlock(x,
                               row = rr,
-                              nrows = tr$nrows[i], 
+                              nrows = tr$nrows[i],
                               firstcol,
                               ncols)
     rowvals <- rowvals[, variables, drop = FALSE]
-    
+
     res <- matrix(NA,
                   nrow = nrow(rowvals),
                   ncol = nlayers(out))
@@ -58,7 +58,7 @@ predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
           newdata[, col] <- factor(newdata[, col])
         }
       }
-      
+
       # predict to this batch of pixels
       p <- predict.graf(object,
                         newdata = newdata,
@@ -73,7 +73,7 @@ predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
         res <- p
       }
     }
-    
+
     if (inMemory) {
       res <- array(res, dim = c(ncol(out), tr$nrows[i], nlayers(out)))
       res <- aperm(res, c(2, 1, 3))
@@ -85,7 +85,7 @@ predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
     pbStep(pb, i)
   }
   pbClose(pb)
-  
+
   if (inMemory) {
     # permute v so that it gets vectorized in the right direction
     v <- aperm(v, c(2, 1, 3))
@@ -98,5 +98,5 @@ predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
     out <- writeStop(out)
   }
   names(out) <- colnames(p)
-  return (out)
+  return(out)
 }
