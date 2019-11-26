0d768e36a82cbae3b5499d5750ca993a580caff9
sjevelazco <sjevelazco@gmail.com>
2019-11-26 14:34:26 -0300

    Function specified by their packages and code formatted

--- R/MESS_&_MOPA_TMLA.R
+++ R/MESS_&_MOPA_TMLA.R
@@ -1,9 +1,9 @@
 MOP <- function(Variables,
-                         RecordsData,
-                         Algorithm,
-                         VarCol,
-                         DirProj,
-                         DirMask) {
+                RecordsData,
+                Algorithm,
+                VarCol,
+                DirProj,
+                DirMask) {
   #Function to calculate MOP metrics
   #Variables: Raster stack in which MESS\MOP will be calculated (from upper function)
   #RecordsData: Species occurrence data\absence (from upper function)
@@ -11,16 +11,16 @@ MOP <- function(Variables,
   #DirProj: Output folder
   
   mop <- function(g_raster, m_occ, percent = 10) {
-    m0 <- na.omit(rasterToPoints(g_raster))
-    m2 <- g <- m0[,-c(1:2)] # G region
+    m0 <- na.omit(raster::rasterToPoints(g_raster))
+    m2 <- g <- m0[, -c(1:2)] # G region
     m0 <- m0[, c(1:2)]
     m1 <- m_occ # data (pres-abs-backgrou) from M region
     set <- c(seq(1, nrow(m2), round(nrow(m2) / 2000)), nrow(m2) + 1)
     mop1 <- lapply(seq_len((length(set) - 1)), function(x) {
       seq_rdist <- set[x]:(set[x + 1] - 1)
-      auclidean <- flexclust::dist2(m2[seq_rdist, ], m1)
+      auclidean <- flexclust::dist2(m2[seq_rdist,], m1)
       meanq <- apply(auclidean, 1, function(x) {
-        d <- quantile(x, probs = percent / 100,
+        d <- stats::quantile(x, probs = percent / 100,
                       na.rm = TRUE)
         return(mean(x[which(x <= d)]))
       })
@@ -30,9 +30,9 @@ MOP <- function(Variables,
     mop_vals <- 1 - (mop_vals / max(mop_vals))
     mopr <- data.frame(m0, mop_vals)
     suppressWarnings({
-      gridded(mopr) <- ~ x + y
+      sp::gridded(mopr) <- ~ x + y
     })
-    mopr <- raster(mopr)
+    mopr <- raster::raster(mopr)
     return(mopr)
   }
   
@@ -40,35 +40,41 @@ MOP <- function(Variables,
   
   #Initialisation
   for (i in 1:length(DirProj)) {
-    foreach (j = 1:length(spN),.packages=c("raster","dismo"),.export="mop")%dopar% {
+    foreach (
+      j = 1:length(spN),
+      .packages = c("raster", "dismo"),
+      .export = "mop"
+    ) %dopar% {
       #Check for existence
-      if (!file.exists(file.path(DirProj[i], paste0(spN[j], "_MOP.tif")))){
-        spOccS <- RecordsData[RecordsData$sp == spN[j],]
-        if(is.null(DirMask)){
+      if (!file.exists(file.path(DirProj[i], paste0(spN[j], "_MOP.tif")))) {
+        spOccS <- RecordsData[RecordsData$sp == spN[j], ]
+        if (is.null(DirMask)) {
           MM <- Variables[[i]][1]
-        }else{
-          MM <- raster(file.path(DirMask, paste0(spN[j], ".tif")))
+        } else{
+          MM <- raster::raster(file.path(DirMask, paste0(spN[j], ".tif")))
         }
-        MP <- rasterize(spOccS[,2:3], MM)
+        MP <- raster::rasterize(spOccS[, 2:3], MM)
         MM[!is.na(MP[])] <- NA
-        if(cellStats(MM, sum)<10000){
-          occM <- raster::sampleRandom(MM, size = cellStats(MM, sum),xy=T)[,1:2]
-          occM <- raster::extract(Variables[[i]],occM)
-        }else{
-          occM <- raster::sampleRandom(MM, size = 10000, xy=T)[,1:2]
-          occM <- raster::extract(Variables[[i]],occM)
+        if (raster::cellStats(MM, sum) < 10000) {
+          occM <-
+            raster::sampleRandom(MM, size = raster::cellStats(MM, sum), xy = T)[, 1:2]
+          occM <- raster::extract(Variables[[i]], occM)
+        } else{
+          occM <- raster::sampleRandom(MM, size = 10000, xy = T)[, 1:2]
+          occM <- raster::extract(Variables[[i]], occM)
         }
-        rm(MP);rm(MM)
+        rm(MP)
+        rm(MM)
         occM <- rbind(spOccS[VarCol], occM[VarCol])
         occM <- na.omit(occM)
         MOPr <- mop(g_raster = Variables[[i]], m_occ = occM)
-        plot(MOPr<0.8)
-        writeRaster(
+        plot(MOPr < 0.8)
+        raster::writeRaster(
           MOPr,
           file.path(DirProj[i], paste0(spN[j], "_MOP.tif")),
           format = "GTiff",
           NAflag = -9999,
-          overwrite=TRUE
+          overwrite = TRUE
         )
       }
     }
--- R/PCAFuturo_TMLA.R
+++ R/PCAFuturo_TMLA.R
@@ -1,94 +1,136 @@
-PCAFuturo<-function(Env,
-                    Dir,
-                    DirP,
-                    Save=''){
-
+PCAFuturo <- function(Env,
+                      Dir,
+                      DirP,
+                      Save = '') {
   #0.Create PCA Folder
-  DirPCA <- file.path(Dir,"PCA")
+  DirPCA <- file.path(Dir, "PCA")
   dir.create(DirPCA)
   #PCA Tables Folder
-  DirPCATab <- file.path(DirPCA,"Tables")
+  DirPCATab <- file.path(DirPCA, "Tables")
   dir.create(DirPCATab)
   
   #Projection Folders
   
-  DirP_PCA <- file.path(dirname(Dir),'Projection_PCA')
+  DirP_PCA <- file.path(dirname(Dir), 'Projection_PCA')
   dir.create(DirP_PCA)
   FoldersProj <- list.files(dirname(DirP[[1]]))
-  FoldersProj <- as.list(file.path(DirP_PCA,FoldersProj))
-  lapply(FoldersProj, function(x) dir.create(x))
+  FoldersProj <- as.list(file.path(DirP_PCA, FoldersProj))
+  lapply(FoldersProj, function(x)
+    dir.create(x))
   
   #1.Present PCA
-  DF<-rasterToPoints(Env)
-  DF<-na.omit(DF)
-  PcaR<-DF[,-c(1:2)]
+  DF <- raster::rasterToPoints(Env)
+  DF <- na.omit(DF)
+  PcaR <- DF[, -c(1:2)]
   
-  means<-colMeans(PcaR)
-  stds<-apply(PcaR,2,sd)
+  means <- colMeans(PcaR)
+  stds <- apply(PcaR, 2, sd)
   
-  #Scale transform 
-  DScale <- data.frame(apply(PcaR,2,scale))
+  #Scale transform
+  DScale <- data.frame(apply(PcaR, 2, scale))
   
-  # PCA 
-  DPca <- prcomp(DScale,retx=TRUE,center=T,scale=T)
-
+  # PCA
+  DPca <- prcomp(DScale,
+                 retx = TRUE,
+                 center = T,
+                 scale = T)
+  
   #Coefficients
-  Coef<-DPca$rotation
-  Coef2 <- data.frame(cbind(Variable=names(Env),Coef))
-  write.table(Coef2,file.path(DirPCATab,"Coeficient.txt"),sep="\t",row.names=F)
+  Coef <- DPca$rotation
+  Coef2 <- data.frame(cbind(Variable = names(Env), Coef))
+  write.table(
+    Coef2,
+    file.path(DirPCATab, "Coeficient.txt"),
+    sep = "\t",
+    row.names = F
+  )
   
   #Cummulative Variance
-  NEixos<-length(summary(DPca)$importance[3,])
-  CumVar<-summary(DPca)$importance[3,]
-  VarEx<-data.frame(CumVar)
-  write.table(VarEx,file.path(DirPCATab,"CumulativeVariance.txt"),sep="\t",row.names=F)
+  NEixos <- length(summary(DPca)$importance[3, ])
+  CumVar <- summary(DPca)$importance[3, ]
+  VarEx <- data.frame(CumVar)
+  write.table(
+    VarEx,
+    file.path(DirPCATab, "CumulativeVariance.txt"),
+    sep = "\t",
+    row.names = F
+  )
   
   #Save PCsthat account for 95% of total variability
-  Eix<-as.data.frame(DPca$x)
-  EixXY<-cbind(DF[,(1:2)],Eix)
-  gridded(EixXY)<- ~x+y
-  PCAPr<-stack(EixXY)
-  PCA.95 <- PCAPr[[1:(sum(VarEx<=0.95)+1)]]
-  if(Save=="Y"){
-    writeRaster(PCA.95,paste(DirPCA,names(PCA.95),sep="/"),bylayer=T,format="GTiff",overwrite=T)
+  Eix <- as.data.frame(DPca$x)
+  EixXY <- cbind(DF[, (1:2)], Eix)
+  sp::gridded(EixXY) <- ~ x + y
+  PCAPr <- raster::stack(EixXY)
+  PCA.95 <- PCAPr[[1:(sum(VarEx <= 0.95) + 1)]]
+  if (Save == "Y") {
+    raster::writeRaster(
+      PCA.95,
+      paste(DirPCA, names(PCA.95), sep = "/"),
+      bylayer = T,
+      format = "GTiff",
+      overwrite = T
+    )
   }
   
   #2.Project PCA
   
   DirP <- as.list(DirP)
-  ProjEX <- lapply(DirP, function(x) unique(file_ext(list.files(x))))
-  form <- c('bil','asc','txt','tif')
-  ProjEX <- unique(unlist(ProjEX)[unlist(ProjEX)%in%form])
-
-  if(any(ProjEX %in% c('asc', 'bil', 'tif'))){
-    ProjT<-lapply(DirP, function(x) brick(stack(file.path(x,list.files(x,paste0('\\.',ProjEX,'$'))))))
+  ProjEX <-
+    lapply(DirP, function(x)
+      unique(file_ext(list.files(x))))
+  form <- c('bil', 'asc', 'txt', 'tif')
+  ProjEX <- unique(unlist(ProjEX)[unlist(ProjEX) %in% form])
+  
+  if (any(ProjEX %in% c('asc', 'bil', 'tif'))) {
+    ProjT <-
+      lapply(DirP, function(x)
+        raster::brick(stack(file.path(
+          x, list.files(x, paste0('\\.', ProjEX, '$'))
+        ))))
   }
-
-  if(any(ProjEX %in% 'txt')){
+  
+  if (any(ProjEX %in% 'txt')) {
     ProjT <- list()
-    for(j in DirP){
-      ProjT[[i]]<-read.table(file.path(DirP[[i]],list.files(DirP[[i]],pattern='.txt'),h=T))
-      gridded(ProjT[[i]])<- ~x+y
-      ProjT[[i]]<-brick(stack(ProjT[[i]]))
+    for (j in DirP) {
+      ProjT[[i]] <-
+        read.table(file.path(DirP[[i]], list.files(DirP[[i]], pattern = '.txt'), h =
+                               T))
+      sp::gridded(ProjT[[i]]) <- ~ x + y
+      ProjT[[i]] <- raster::brick(raster::stack(ProjT[[i]]))
     }
   }
-
-  ProjE<-lapply(ProjT, function(x) rasterToPoints(x))
-  ProjE<-lapply(ProjE, function(x) na.omit(x))
-  ProjER <- lapply(ProjE, function(z) z[,!(colnames(z) %in% c("x", "y"))])
   
-  scale<-lapply(ProjER, function(x) sweep(x,2,means))
-  scale<-lapply(scale, function(x) x %*% diag(1/stds))
-  PCAFut<-lapply(scale, function(x) x %*% Coef)
-  PCAFut <- lapply(PCAFut, function(x) data.frame(cbind(ProjE[[1]][,(1:2)],x)))
+  ProjE <- lapply(ProjT, function(x)
+    raster::rasterToPoints(x))
+  ProjE <- lapply(ProjE, function(x)
+    na.omit(x))
+  ProjER <-
+    lapply(ProjE, function(z)
+      z[, !(colnames(z) %in% c("x", "y"))])
+  
+  scale <- lapply(ProjER, function(x)
+    sweep(x, 2, means))
+  scale <- lapply(scale, function(x)
+    x %*% diag(1 / stds))
+  PCAFut <- lapply(scale, function(x)
+    x %*% Coef)
+  PCAFut <-
+    lapply(PCAFut, function(x)
+      data.frame(cbind(ProjE[[1]][, (1:2)], x)))
   PCAFut.95 <- list()
-  for(j in 1:length(PCAFut)){
-    gridded(PCAFut[[j]])<- ~x+y
-    PCAFut[[j]]<-stack(PCAFut[[j]])
+  for (j in 1:length(PCAFut)) {
+    sp::gridded(PCAFut[[j]]) <- ~ x + y
+    PCAFut[[j]] <- raster::stack(PCAFut[[j]])
     names(PCAFut[[j]]) <- names(PCAPr)
-    PCAFut.95[[j]] <- PCAFut[[j]][[1:nlayers(PCA.95)]]
-    if(Save=="Y"){
-      writeRaster(PCAFut.95[[j]],paste(FoldersProj[[j]],names(PCAFut.95[[j]]),sep="/"),bylayer=T,format="GTiff",overwrite=T)
+    PCAFut.95[[j]] <- PCAFut[[j]][[1:raster::nlayers(PCA.95)]]
+    if (Save == "Y") {
+      raster::writeRaster(
+        PCAFut.95[[j]],
+        paste(FoldersProj[[j]], names(PCAFut.95[[j]]), sep = "/"),
+        bylayer = T,
+        format = "GTiff",
+        overwrite = T
+      )
     }
   }
   return(PCAFut.95)
