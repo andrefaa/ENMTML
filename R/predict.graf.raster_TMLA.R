6252369887ec5b044a228ab2b088ae04c06d1297
sjevelazco <sjevelazco@gmail.com>
2019-11-26 16:48:33 -0300

    graf.raster corrected

--- R/predict.graf.raster_TMLA.R
+++ R/predict.graf.raster_TMLA.R
@@ -19,10 +19,10 @@ predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
     }
   }
   ncols <- ncol(out)
-  
+
   firstrow <- 1
   firstcol <- 1
-  
+
   if (!canProcessInMemory(out, 3)) {
     filename <- raster::rasterTmpFile()
     out <- raster::writeStart(out, filename = filename, ...)
@@ -31,20 +31,20 @@ predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
     v <- array(NA, dim = c(nrow(out), ncol(out), nlayers(out)))
     inMemory <- TRUE
   }
-  
+
   tr <- raster::blockSize(out, n = raster::nlayers(x) + 2)
   pb <- raster::pbCreate(tr$n)
-  
+
   for (i in 1:tr$n) {
     rr <- firstrow + tr$row[i] - 1
-    
+
     rowvals <- raster::getValuesBlock(x,
                                       row = rr,
                                       nrows = tr$nrows[i],
                                       firstcol,
                                       ncols)
     rowvals <- rowvals[, variables, drop = FALSE]
-    
+
     res <- matrix(NA,
                   nrow = nrow(rowvals),
                   ncol = raster::nlayers(out))
@@ -57,7 +57,7 @@ predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
           newdata[, col] <- factor(newdata[, col])
         }
       }
-      
+
       # predict to this batch of pixels
       p <- predict.graf(
         object,
@@ -74,7 +74,7 @@ predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
         res <- p
       }
     }
-    
+
     if (inMemory) {
       res <- array(res, dim = c(ncol(out), tr$nrows[i], nlayers(out)))
       res <- aperm(res, c(2, 1, 3))
@@ -83,10 +83,10 @@ predict.graf.raster <- function (object, x, type, CI, maxn, ...) {
     } else {
       out <- raster::writeValues(out, res, tr$row[i])
     }
-    pbraster::Step(pb, i)
+    raster::pbStep(pb, i)
   }
   raster::pbClose(pb)
-  
+
   if (inMemory) {
     # permute v so that it gets vectorized in the right direction
     v <- aperm(v, c(2, 1, 3))
