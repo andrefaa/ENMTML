2f3276417a782b7d6438acbe8d0565c90fe4810c
Santiago J.E. Velazco <sjevelazco@gmail.com>
2020-08-31 12:35:14 -0300

    Revert "Merge pull request #295 from andrefaa/Santi_Branch"
    This reverts commit 58a51bec8cbb5f5a20ea07a999706e23e985d349.

--- R/Occ_Unicas_TMLA.R
+++ R/Occ_Unicas_TMLA.R
@@ -4,12 +4,12 @@ Occ_Unicas_TMLA <- function(env,
   spN <- names(occ.xy)
   occ.v <-
     lapply(occ.xy, function(x)
-      data.frame(raster::extract(env, x, cellnumber = T))[1])
+      raster::extract(env, x, cellnumber = T))
   occ.v <- mapply(cbind, occ.xy, occ.v, SIMPLIFY = F)
   occ.v <- lapply(occ.v, function(x)
     stats::na.omit(x))
   occ.v <- lapply(occ.v, function(x)
-    x <- x[!duplicated(x$cells), -3])
+    x[!duplicated(x$cells), ])
   uni <-
     data.frame(Species = spN,
                UniqueOcc = sapply(occ.v, function(x)
